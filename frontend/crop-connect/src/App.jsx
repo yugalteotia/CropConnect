149221c9b3d88dd27e653c81a171353586c8f7b2
Kshitij <kshitijtripathi123@gmail.com>
2024-08-14 21:41:41 +0530

    Fixed Errors in CartService

--- frontend/crop-connect/src/App.jsx
+++ frontend/crop-connect/src/App.jsx
@@ -2,17 +2,11 @@ import { useState } from 'react'
 import Home from './components/home/Home'
 import NavigationBar from './components/home/NavigationBar'
 import SignUpForm from './components/SignUpForm'
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
-import SignInForm from './components/SigninForm';
-import AboutUs from './components/AboutUs';
-import Footer from './components/home/Footer';
-=======
 import Merchant from './components/merchant/Merchant'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Footer from './components/home/Footer'
-// import NavigationBar from './components/home/NavigationBar'
->>>>>>> 73bcb9c409cf1d74bb61cb2cf40cf36c1fe7da29
+import AboutUs from './components/AboutUs'
+import SignInForm from './components/SigninForm'
 
 const ConditionalNavigationBar = () => {
   const location = useLocation();
@@ -31,35 +25,13 @@ const ConditionalNavigationBar = () => {
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/signin" element={<SignInForm />} />
+          <Route path="/merchant" element={<Merchant />} />
         </Routes>
         <Footer />
       </Router>
       // </>
     )
-  }
-
-<<<<<<< HEAD
-  export default App
-=======
-  return (
-    <>
-    
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUpForm />} />
-        
-        <Route path="/merchant" element={<Merchant />} />
-      </Routes>
-    </Router>
-    <Footer />
-
-
-
-    </>
-  )
+  
 }
 
 export default App
->>>>>>> 73bcb9c409cf1d74bb61cb2cf40cf36c1fe7da29
