aa51a4e74ab73d1af6f7e9a1083e3c68d93cb5d7
Asmita-Mahajan <asmitamahajan2000@gmail.com>
2024-08-15 05:27:53 +0530

    CartPage added

--- frontend/crop-connect/src/App.jsx
+++ frontend/crop-connect/src/App.jsx
@@ -1,62 +1,35 @@
-import { useState } from 'react'
-import Home from './components/home/Home'
-import NavigationBar from './components/home/NavigationBar'
-import SignUpForm from './components/SignUpForm'
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CartPage from './components/CartPage'
-=======
-import Merchant from './components/merchant/Merchant'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Footer from './components/home/Footer'
-import AboutUs from './components/AboutUs'
-import SignInForm from './components/SigninForm'
+
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./components/home/Home";
+import NavigationBar from "./components/home/NavigationBar";
+import SignUpForm from "./components/SignUpForm";
+import CartPage from "./components//home/CartPage";
+import Merchant from "./components/merchant/Merchant";
+import Footer from "./components/home/Footer";
+import AboutUs from "./components/AboutUs";
+import SignInForm from "./components/SignInForm";
+
 
 const ConditionalNavigationBar = () => {
   const location = useLocation();
-  // Hide NavigationBar on the home page
-  return location.pathname !== '/' ? <NavigationBar /> : null;
+  return location.pathname !== "/" ? <NavigationBar /> : null;
 };
->>>>>>> 1fe60bd554e4d9385a7e2bd39b8fc3e8ad26ec4d
 
-  function App() {
-
-<<<<<<< HEAD
 function App() {
-
   return (
-    <>
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<LoginForm />} />
-        <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-    </Router>
-
-
-
-    </>
-  )
-=======
-    return (
-      // <>
-      <Router>
+    <Router>
       <ConditionalNavigationBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/signin" element={<SignInForm />} />
-          <Route path="/merchant" element={<Merchant />} />
-        </Routes>
-        <Footer />
-      </Router>
-      // </>
-    )
-  
->>>>>>> 1fe60bd554e4d9385a7e2bd39b8fc3e8ad26ec4d
+      <Routes>
+       
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/signin" element={<SignInForm />} />
+        <Route path="/" element={<CartPage />} />
+        <Route path="/merchant" element={<Merchant />} /> 
+              </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
