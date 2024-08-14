9bf54f5c73e659f6d0da2db736491755613001d1
Kshitij <kshitijtripathi123@gmail.com>
2024-08-14 20:49:15 +0530

    Added getallCartItem method

--- backend/src/main/java/com/cropconnect/repository/CartItemRepository.java
+++ backend/src/main/java/com/cropconnect/repository/CartItemRepository.java
@@ -1,9 +1,12 @@
 package com.cropconnect.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cropconnect.entities.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
 	
+	List<CartItem> findByCartId(Integer cartId);
 }
